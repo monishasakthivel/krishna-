fc288ac7665c82205d1fc7c681d6a7fa4e0db33f
monishasakthivel <93466227+monishasakthivel@users.noreply.github.com>
2021-11-01 12:38:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Dear Amma !</h1>
+      <h2>I haven't seen you for past few years!</h2>
+      <h3>I have been working in abroad,Now the time is for you Amma..!</h3> 
+      <h4>Hereafter,I won't leave you ma!</h4>
+      <p>I'm coming to you amma! luv you :)</p>
+    </div>
+  );
+}
